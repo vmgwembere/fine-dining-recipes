f48c870252e850a745fdd4e276ad09543b0ff11e
Veronica Gwembere <vmgwembere@gmail.com>
2018-11-11 19:44:20 +0000

    Updated port

--- app.py
+++ app.py
@@ -97,5 +97,5 @@ def new_category():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
-        port=int(os.environ.get('PORT' )),
+        port=int(os.environ.get('PORT',5000 )),
         debug=False)

09170ed3f843a610719bc27d749990da4da75c8b
Veronica Gwembere <vmgwembere@gmail.com>
2018-11-11 19:30:49 +0000

    Fixed port issue

--- app.py
+++ app.py
@@ -96,6 +96,6 @@ def new_category():
        
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', '0.0.0.0'),
+    app.run(host='0.0.0.0',
         port=int(os.environ.get('PORT' )),
-        debug=True)
+        debug=False)

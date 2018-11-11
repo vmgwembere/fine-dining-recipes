672a5f0b9a80ddfff5b819a9322e3af551e0549a
Veronica Gwembere <vmgwembere@gmail.com>
2018-11-11 19:26:35 +0000

    Updated host

--- app.py
+++ app.py
@@ -96,6 +96,6 @@ def new_category():
        
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+        port=int(os.environ.get('PORT' )),
         debug=True)
